b5d900c67cbd61970288baf0757ed15257b610a4
Jeremiah Okoduwa <jerryokoduwa@gotmail.co.uk>
2022-04-15 14:35:09 +0100

    update platform api service

---
+++ Models/Platform.cs
@@ -0,0 +1,21 @@
+using System.Security.AccessControl;
+using System.ComponentModel.DataAnnotations;
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Publisher { get; set; }
+        public string? Cost { get; set; }
+        
+        
+        
+            
+        
+        
+        
+        
+    }
+}
